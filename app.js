c4dd21816a0e985282dfb3d1a3933ea67e12962c
jtmar28 <justinmartinez0123@gmail.com>
2018-11-16 23:08:31 -0500

    Added functions to store and add items to Local Storage

--- app.js
+++ app.js
@@ -9,12 +9,43 @@ const search = document.querySelector('#search');
 loadAllEventListeners();
 
 function loadAllEventListeners(){
+    document.addEventListener('DOMContentLoaded',getGroceryItemsFromLS);
     form.addEventListener('submit', addGroceryItem);
     groceryList.addEventListener('click', deleteGroceryItem);
     search.addEventListener('keyup', filterGroceryList);
     clear.addEventListener('click', clearAllGroceryListItems);
 }
-
+function StoreGroceryItemsIntoLS(item){
+    let items;
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    items.push(item);
+    localStorage.setItem('items', JSON.stringify(items));
+}
+function getGroceryItemsFromLS(){
+    let items;
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    items.forEach(function(item){
+        const li = document.createElement('li');
+        //Add Class name
+        li.className = 'collection-item';
+        //Append input into the li
+        li.appendChild(document.createTextNode(item));
+        //Create delete button
+        const link = document.createElement('a');
+        link.className = 'delete secondary-content';
+        link.innerHTML = '<i class="material-icons prefix">delete</i>';
+        li.appendChild(link);
+        groceryList.appendChild(li);
+    });
+}
 function addGroceryItem(e){
     if(input.value === ''){
         alert('Please Enter Grocery Item');
@@ -31,6 +62,8 @@ function addGroceryItem(e){
         link.innerHTML = '<i class="material-icons prefix">delete</i>';
         li.appendChild(link);
         groceryList.appendChild(li);
+        //Store into ls
+        StoreGroceryItemsIntoLS(input.value);
         //Clear Text Value
         input.value = '';
     }
@@ -41,16 +74,18 @@ function deleteGroceryItem(e){
         e.target.parentElement.parentElement.remove();
     }
 }
+function deleteGroceryItemFromLS(){
+
+}
 function filterGroceryList(e){
     const text = e.target.value.toLowerCase();
-    document.querySelectorAll('.collection-item').forEach(function(task){
-        const item = task.firstChild.textContent;
-        if(item.toLowerCase().indexOf(text) != -1){
-            task.style.display = 'block';
+    document.querySelectorAll('.collection-item').forEach(function(item){
+        const items = item.firstChild.textContent;
+        if(items.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
         }else{
-            task.style.display = 'none';
+            item.style.display = 'none';
         }
-
     });
 }
 function clearAllGroceryListItems(){
